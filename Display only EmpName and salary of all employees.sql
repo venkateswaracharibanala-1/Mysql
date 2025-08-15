1c13abda783d9fa7365bd957ae2cf15b38530d2f
venkateswaracharibanala-1 <venkateswaracharibanala@gmail.com>
2025-08-15 23:03:54 +0530

    Add files via upload

---
+++ Display only EmpName and salary of all employees.sql
@@ -0,0 +1,9 @@
+ SELECT EMP_Name,Salary FROM employee;
++----------+--------+
+| EMP_Name | Salary |
++----------+--------+
+| john     |  50000 |
+| Alice    |  60000 |
+| Bob      |  55000 |
+| Carol    |  45000 |
++----------+--------+
